3321f0b2616940674185d8689e1a69202dca627a
DanVadAnt <122672499+DanVadAnt@users.noreply.github.com>
2023-10-24 20:00:14 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Cryptor
+{
+    /// <summary>
+    /// Класс содержащий методы encrypt decrypt
+    /// </summary>
+    public class Crypt {
+        public string CryptInfo()
+        {
+            string info = "Шифр замены";
+            return info;
+        }
+    }
+    public class ShiftCryptor:Crypt 
+    {
+        /// <summary>
+        /// Шифрует текст
+        /// </summary>
+        /// <param name="openText">Открытый текст</param>
+        /// <param name="alphabet">Алфавит</param>
+        /// <param name="delta">Сдвиг</param>
+        /// <returns>Возвращает зашифрованный текст</returns>
+        public string encrypt(string openText,string alphabet,int delta) 
+        {
+            StringBuilder shifrText = new StringBuilder(openText.Length);
+            for(int i = 0; i < openText.Length; i++)
+            {
+                shifrText.Append(alphabet[(alphabet.IndexOf(openText[i]) + delta) % alphabet.Length]);
+            }
+            File.WriteAllText("text.txt", shifrText.ToString());
+            return shifrText.ToString();
+        }
+        /// <summary>
+        /// Расшифровывает текст
+        /// </summary>
+        /// <param name="shifrText">Шифротекст</param>
+        /// <param name="alphabet">Алфавит</param>
+        /// <param name="delta">Сдвиг</param>
+        /// <returns>Возвращает рассшифрованный текст</returns>
+        public string decrypt(string shifrText, string alphabet, int delta)
+        {
+            StringBuilder openText = new StringBuilder(shifrText.Length);
+            for (int i = 0; i < shifrText.Length; i++)
+            {
+                openText.Append(alphabet[(alphabet.IndexOf(shifrText[i]) - delta+alphabet.Length) % alphabet.Length]);
+            }
+            File.WriteAllText("text.txt", openText.ToString());
+            return openText.ToString();
+        }
+    }
+    public class ReplacementCryptor : Crypt
+    {
+        /// <summary>
+        /// Шифрует текст
+        /// </summary>
+        /// <param name="openText">Открытый текст</param>
+        /// <param name="alphabet">Алфавит</param>
+        /// <param name="delta">Сдвиг</param>
+        /// <returns>Возвращает зашифрованный текст</returns>
+        public string encrypt(string openText, Dictionary<char,char> replacementTable)
+        {
+            StringBuilder shifrText = new StringBuilder(openText.Length);
+            for (int i = 0; i < openText.Length; i++)
+            {
+                replacementTable.TryGetValue(openText[i], out char c);
+                shifrText.Append(c);
+            }
+            File.WriteAllText("text.txt", shifrText.ToString());
+            return shifrText.ToString();
+        }
+        /// <summary>
+        /// Расшифровывает текст
+        /// </summary>
+        /// <param name="shifrText">Шифротекст</param>
+        /// <param name="alphabet">Алфавит</param>
+        /// <param name="delta">Сдвиг</param>
+        /// <returns>Возвращает рассшифрованный текст</returns>
+        public string decrypt(string shifrText, Dictionary<char,char> replacementTable)
+        {
+            StringBuilder openText = new StringBuilder(shifrText.Length);
+            for (int i = 0; i < shifrText.Length; i++)
+            {
+
+                replacementTable.TryGetValue(shifrText[i], out char c);
+                openText.Append(c);
+            }
+            File.WriteAllText("text.txt", openText.ToString());
+            return openText.ToString();
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using CommandLine;
+using Cryptor;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic;
+using System.IO;
+
+namespace laba1
+{
+
+    public class Options
+    {
+        [Option('s', "shifr_type", Required = true, HelpText = "Тип шифра")]
+        public string ShifrMode { get; set; }
+
+        [Option('t', "text", Required = true, HelpText = "Открытый текст")]
+        public string Text { get; set; }
+
+        [Option('a', "alphabet", Required = true, HelpText = "алфавит")]
+        public string Alphabet { get; set; }
+
+        [Option('m', "mode", Required = true, HelpText = "mode: encrypt or decrypt")]
+        public string Mode { get; set; }
+
+        [Option('d', "delta", Required = false, HelpText = "delta")]
+        public int Delta { get; set; }
+
+        [Option('b', "table", Required = false, HelpText = "Таблица замен")]
+        public string ReplacementTable { get; set; }
+    }
+  
+    class Program
+    {
+        static public Dictionary<char,char> mixDict(string alphabet)
+        {
+            string values = String.Copy(alphabet);
+            values = new string(alphabet.ToCharArray().OrderBy(x => Guid.NewGuid()).ToArray());
+            Dictionary<char,char> replacementTable = alphabet.Zip(values, (s, i) => new { s, i })
+                          .ToDictionary(item => item.s, item => item.i);
+            File.WriteAllText("rTable.txt", replacementTable.ToString());
+            return replacementTable;
+        } 
+        static public Dictionary<char, char> keyToValue(Dictionary<char,char> dict)
+        {
+            Dictionary<char, char> result = new Dictionary<char, char>();
+            foreach (char key in dict.Keys)
+            {;
+                dict.TryGetValue(key, out char c);
+                result.Add(c, key);
+            }
+            return result;
+        }
+        static public Dictionary<char, char> alphabetAndTableToDictionary(string alphabet, string replacementTable)
+        {
+            Dictionary<char, char> result = new Dictionary<char, char>();
+            for (int i = 0; i < alphabet.Length-1; i++)
+            {
+                result.Add(alphabet[i], replacementTable[i]);
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+          var result = Parser.Default.ParseArguments<Options>(args)
+        .WithParsed<Options>(o =>
+       {
+                
+       });
+            var options = result.Value;
+            string table = options.ReplacementTable;
+            string text = options.Text;
+            string alphabet = options.Alphabet;
+            int delta = options.Delta;
+            string mode = options.Mode;
+            string output = "";
+            string shifrMode = options.ShifrMode;
+            Dictionary<char, char> replacementTable = alphabetAndTableToDictionary(alphabet,table);
+            switch (shifrMode)
+            {
+                case "Replacement":
+                    {
+                        ReplacementCryptor Cryptor = new ReplacementCryptor();
+                        switch (mode)
+                        {
+                            case "encrypt":
+                                output = Cryptor.encrypt(text, replacementTable);
+                                Console.WriteLine(output);
+
+                                break;
+                            case "decrypt":
+                                replacementTable = keyToValue(replacementTable);
+                                output = Cryptor.decrypt(text, replacementTable);
+                                Console.WriteLine(output);
+                                break;
+                        }
+                        break;
+                    }
+                case "Shift":
+                    {
+                        ShiftCryptor Cryptor = new ShiftCryptor();
+                        switch (mode)
+                        {
+                            case "encrypt":
+                                output = Cryptor.encrypt(text, alphabet, delta);
+                                Console.WriteLine(output);
+                                break;
+                            case "decrypt":
+                                output = Cryptor.decrypt(text, alphabet, delta);
+                                Console.WriteLine(output);
+                                break;
+                        }
+                        break;
+                    }
+            }
+            
+
+            
+        }
+    }
+}
